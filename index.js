7eb1106b85324ea05a3cb062dda760a089a3e563
SabinoWS <sabino.th@hotmail.com>
2024-04-30 22:03:18 -0300

    Adiciona início de fetch para cadastro

--- index.js
+++ index.js
@@ -2,6 +2,45 @@ import "./scss/index.scss"
 
 console.log("Iniciando contexto global: Página de usuário...")
 
+const botaoCadastrar = document.getElementById("botaoCadastrar")
+botaoCadastrar.addEventListener("click", executarCadastrar)
+
+async function executarCadastrar() {
+    // Cadastrar livros na API de livros
+
+    // Entrada de dados
+    const titulo = document.getElementById("titulo").value
+    const descricao = document.getElementById("descricao").value
+
+    // Processamento
+    const sucesso = await cadastrarLivroNaAPI(titulo, descricao)
+
+    // Saída de dados
+    
+}
+
+async function cadastrarLivroNaAPI(titulo, descricao) {
+    const url = "https://api-aula.up.railway.app/livros"
+    const dados = {
+        title: titulo,
+        description: descricao
+    }
+
+    const resposta = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dados)
+    })
+
+    if (resposta.ok) {
+        return true 
+    } else {
+        return false
+    }
+}
+
 // Exercício 1
 // 1 - Adicionar evento de click no botão de execução
 // 2 - Criar função de início do processo
